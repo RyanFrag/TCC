a53cdb224c1e41225393cfd4340fcd320ff67dd8
Oicanji <51388779+Oicanji@users.noreply.github.com>
2023-10-02 11:43:25 -0300

    a12

--- website/static/game/controller/events.js
+++ website/static/game/controller/events.js
@@ -1,9 +1,7 @@
-import { k } from "../../game.js"
-
 let event_listener = new EventTarget();
 
 function post_event(name){
-    console.log("[EVENT] Event: " + name);
+    console.log("[EMMIT] Event: " + name);
     event_listener.dispatchEvent(new CustomEvent(name));
 }
 function call_event(event, callback){
@@ -12,10 +10,16 @@ function call_event(event, callback){
 function remove_all_events(){
     event_listener = new EventTarget();
 }
+function remove_event(event, callback){
+    console.log("[EVENT] Event removed: " + event);
+    event_listener.removeEventListener(event, callback);
+}
+
 const events = {
     emit: post_event,
     listen: call_event,
-    clear: remove_all_events
+    clear: remove_all_events,
+    remove: remove_event
 }
 
 export default events;
