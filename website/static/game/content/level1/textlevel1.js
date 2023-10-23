3b5adf01e6a49a3407d30a8df4cf15621706a1b8
RyanFrag <92130127+RyanFrag@users.noreply.github.com>
2023-10-23 17:56:05 -0300

    Uns ajustes antes de ir pra aula

--- website/static/game/content/level1/textlevel1.js
+++ website/static/game/content/level1/textlevel1.js
@@ -1,8 +1,8 @@
 export const textLines = [
-    { text: " Finalmente, após uma ardúa Jornada os Personagens chegam\n ao seu Destino, o local onde está o Cetro da Sabedoria.", character: "Narrador" },
-    { text: "Então deve ser aqui... eu só ouvi lendas desse\n lugar, e você, já esteve aqui antes?",  sprite: "static", character: "sacerdotisa"},
-    { text: "  Não, nunca estive... só ouvi lendas, sei que é mais seguro\n que quando Teseu esteve aqui, isso eu sei.",  sprite: "static", character: "hero" },
-    { text: "   Acha que o Minotauro ainda anda por esse labirinto?\n Isso é uma história antiga, não é?\n", sprite: "static", character: "sacerdotisa" },
-    { text: "Se Teseu não mentiu, ele não deve estar aqui, mas vamos,\n se o encontramos daremos um jeito.\n", sprite: "static", character: "hero" },
+    { text: "Finalmente, após uma ardúa Jornada\n os Personagens chegam ao seu Destino,\n o local onde está o Cetro da Sabedoria.", character: "Narrador" },
+    { text: "Então deve ser aqui... \n eu só ouvi lendas desse lugar, e você?\n já esteve aqui antes?",  sprite: "static", character: "sacerdotisa"},
+    { text: "Não, nunca estive... \n só ouvi lendas, sei que é mais seguro\n que quando Teseu esteve aqui, isso eu sei.",  sprite: "static", character: "hero" },
+    { text: "Acha que o Minotauro\n ainda anda por esse labirinto?\n Isso é uma história antiga, não é?", sprite: "static", character: "sacerdotisa" },
+    { text: "Se Teseu não mentiu, ele não deve estar aqui,\n mas vamos, se o encontramos daremos um jeito.", sprite: "static", character: "hero" },
 ];
 
