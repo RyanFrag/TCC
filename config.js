d92294d300054fa34ccd04999e1e0ceebe0a6574
RyanFrag <92130127+RyanFrag@users.noreply.github.com>
2023-10-24 21:49:55 -0300

    espero ser isso

---
+++ config.js
@@ -0,0 +1,96 @@
+export const level5Config = {
+    playerPositionX: 600,
+    playerPositionY: 1040,
+    nbLives: 3,
+    playerSpeed: 200,
+    columsPositions: [
+        () => vec2(200,220),
+        () => vec2(200,280),
+        () => vec2(200,340),
+        () => vec2(200,400),
+        () => vec2(200,460),
+        () => vec2(200,520),
+        () => vec2(200,580),
+
+    ],
+ 
+    columsTypes: [
+        () => "upcollum",
+        () => "midle1",
+        () => "midle3",
+        () => "bosscollum",
+        () => "midle2",
+        () => "midle1",
+        () => "basecollum",
+    ],
+
+    columsPositions2: [
+        () => vec2(930,220),
+        () => vec2(930,280),
+        () => vec2(930,340),
+        () => vec2(930,400),
+        () => vec2(930,460),
+        () => vec2(930,520),
+        () => vec2(930,580),
+ 
+    ],
+
+    columsTypes2: [
+        () => "upcollum",
+        () => "midle1",
+        () => "midle3",
+        () => "bosscollum",
+        () => "midle2",
+        () => "midle1",
+        () => "basecollum",
+    ],
+    columsPositions3: [
+        () => vec2(200,1080),
+        () => vec2(200,1140),
+        () => vec2(200,1200),
+        () => vec2(200,1260),
+        () => vec2(200,1320),
+        () => vec2(200,1380),
+        () => vec2(200,1420),
+    ],
+
+    columsTypes3: [
+        () => "upcollum",
+        () => "midle5",
+        () => "midle4",
+        () => "bosscollum",
+        () => "midle3",
+        () => "midle2",
+        () => "basecollum",
+    ],
+
+    columsPositions4: [
+        () => vec2(930,1080),
+        () => vec2(930,1140),
+        () => vec2(930,1200),
+        () => vec2(930,1260),
+        () => vec2(930,1320),
+        () => vec2(930,1380),
+        () => vec2(930,1420),
+ 
+    ],
+
+    columsTypes4: [
+        () => "upcollum",
+        () => "midle1",
+        () => "midle3",
+        () => "bosscollum",
+        () => "midle3",
+        () => "midle1",
+        () => "basecollum",
+    ],
+
+
+
+    cameraLeftBound : 625,
+    cameraRightBound : 610,
+    cameraTopBound : 360,
+    cameraBottomBound : 1220,
+
+}
+
